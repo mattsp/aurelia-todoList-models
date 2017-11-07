6d16336b5435a97109e263c83910a4f8a011cdb9
gianmatteo.spoto <gianmatteo.spoto@gaming1.com>
2017-11-07 09:42:24 +0100

    add clean task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,12 @@ var ts = require('gulp-typescript');
 var concat = require('gulp-concat');
 var tsProject = ts.createProject('tsconfig.json')
 var merge = require('merge2');
+var clean = require('gulp-clean');
+
+gulp.task('clean', function() {
+    return gulp.src('build/**/*')
+    .pipe(clean())
+})
 
 gulp.task('scripts', function() {
     var tsResult = gulp.src('src/**/*.ts')
@@ -15,3 +21,5 @@ gulp.task('scripts', function() {
             .pipe(gulp.dest('build/js'))
         ]);
 });
+
+gulp.task('build', gulp.series('clean', 'scripts'));
